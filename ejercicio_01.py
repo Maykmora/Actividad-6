70ce7534eea3e9c0e58a7efc8bf81ce1cc0f8c00
Maykmora <maykmora12@gmail.com>
2025-07-24 09:40:32 -0600

    commit 3

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,11 +1,34 @@
 def menu():
     print("\n--MENÚ--")
-    print("1.Ingresar n numeros")
+    print("1.Ingresar n números")
     print("2.Calcular el área de un triangulo")
     print("3.Verificar si un numero es par o impar")
     print("4.Calcular el promedio de n calificaciones")
-    print("5.Calcular el numero mayor y el menor de n numeros ")
-    print("5.Salir del programa")
+    print("5.Calcular el numero mayor y el menor de n números ")
+    print("6.Salir del programa")
 
 
-menu()
+while True:
+    menu()
+    option=input("\nSeleccione una opción del menú: ")
+
+    match option:
+        case "1":
+            print("Hola")
+        case "2":
+            print()
+
+        case "3":
+            print()
+
+        case "4":
+            print()
+
+        case "5":
+            print()
+
+        case "6":
+            print("Saliendo del programa")
+            break
+        case _:
+            print("Opción invalida, inténtelo de nuevo")
