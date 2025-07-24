f0ddc2eb8506657891236b70f7f0870eb4f0630e
Maykmora <maykmora12@gmail.com>
2025-07-24 09:36:04 -0600

    commit 2

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -2,3 +2,10 @@ def menu():
     print("\n--MENÚ--")
     print("1.Ingresar n numeros")
     print("2.Calcular el área de un triangulo")
+    print("3.Verificar si un numero es par o impar")
+    print("4.Calcular el promedio de n calificaciones")
+    print("5.Calcular el numero mayor y el menor de n numeros ")
+    print("5.Salir del programa")
+
+
+menu()
